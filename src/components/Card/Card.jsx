b1e607bfb1fef42e3463c80c80a53f5ee36d996b
Dennis Francis <dnfrancis22@gmail.com>
2021-02-02 13:10:24 -0500

    updated card to take in props.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
     return (
       
  <>
- <div className="card" style={{width: "18rem"}}>
-  <img src="..." class="card-img-top" alt="..."/>
+ <div className="card" style={{width: "50rem"}}>
+  <img src={props.image} class="card-img-top" alt={props.title}/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.description}</p>
   </div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">Cras justo odio</li>
-    <li className="list-group-item">Dapibus ac facilisis in</li>
-    <li className="list-group-item">Vestibulum at eros</li>
-  </ul>
   <div className="card-body">
-    <a href="/" className="card-link">Card link</a>
-    <a href="/" claclassNamess="card-link">Another link</a>
+    <a href={props.github} className="card-link">Github</a>
+    <a href={props.deployedSite} className="card-link">Deployed Site</a>
   </div>
 </div>
  </>
--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../../components/Card/Card"
+import Card from "../../components/Card/Card";
 
 const Portfolio = () => {
   return (
@@ -7,12 +7,20 @@ const Portfolio = () => {
       <div className="row">
         <div className="col">
           <h1>Portfolio</h1>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          <Card
+            image="./assets/theInspirator.png"
+            title="The Inspirator"
+            description="The Inspirator is an application to help unlock creativity for persons needing a fresh approach. This site provides a different perspective to help continue or initiate a project, from writing a song to plotting a story. "
+            github="https://github.com/dnfrancis22/GT-Project1"
+            deployedSite="https://dnfrancis22.github.io/GT-Project1/"
+          />
+          <Card
+            image="./assets/theInspirator.png"
+            title="The Inspirator"
+            description="The Inspirator is an application to help unlock creativity for persons needing a fresh approach. This site provides a different perspective to help continue or initiate a project, from writing a song to plotting a story. "
+            github="https://github.com/dnfrancis22/GT-Project1"
+            deployedSite="https://dnfrancis22.github.io/GT-Project1/"
+          />
         </div>
       </div>
     </div>
