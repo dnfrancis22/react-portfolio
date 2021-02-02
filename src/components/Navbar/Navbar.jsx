7ddb91b24c2836989eda1452c41b494c2e2a1ce1
Dennis Francis <dnfrancis22@gmail.com>
2021-02-02 11:42:50 -0500

    modified the nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function NavBar(props) {
   return (
-    <ul className="nav nav-tabs">
+    <>
+      {/* <ul className="nav nav-tabs">
       <li className="nav-item">
         <a
           href="#About"
@@ -30,7 +31,67 @@ function NavBar(props) {
           Portfolio
         </a>
       </li>
-    </ul>
+    </ul> */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div>
+          {/* <a
+            className="nav-item"
+            href="#About"
+            onClick={() => props.handlePageChange("About")}
+            className={
+              props.currentPage === "About" ? "nav-link active" : "nav-link"
+            }
+          >
+            Dennis Francis
+          </a> */}
+          <h3>Dennis Francis</h3>
+        </div>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ml-auto">
+            <a
+              className="nav-item"
+              href="#Contact"
+              onClick={() => props.handlePageChange("Contact")}
+              className={
+                props.currentPage === "Contact" ? "nav-link active" : "nav-link"
+              }
+            >
+              Contact
+            </a>
+            <a
+              className="nav-item"
+              href="#Portfolio"
+              onClick={() => props.handlePageChange("Portfolio")}
+              className={
+                props.currentPage === "Portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
+            </a>
+            <a
+              className="nav-item"
+              href="#About"
+              onClick={() => props.handlePageChange("About")}
+              className={
+                props.currentPage === "About" ? "nav-link active" : "nav-link"
+              }
+            >
+              About
+            </a>
+            
+            <a
+              className="nav-link"
+              href="https://docs.google.com/document/d/1MZ_OTMBpeteeMLyJbuGzgmT9NBKnAI1Qt-k5Pt69K1Y/edit?usp=sharing"
+              download
+            >
+              Resume
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
