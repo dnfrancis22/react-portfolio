983839ff668258b2708f2213a43d0270112122bc
Dennis Francis <dnfrancis22@gmail.com>
2021-02-02 12:40:54 -0500

    created generic card component

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import Card from "../../components/Card/Card"
 
 const Portfolio = () => {
-    return (
-        <div>
-           <h1>this is my portfolio page</h1> 
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Portfolio</h1>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
