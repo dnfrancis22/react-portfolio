b54a7a06c0b8798c7f1f89c2b5fceeb35d84bf93
Dennis Francis <dnfrancis22@gmail.com>
2021-02-03 02:55:45 -0500

    updated background image and fixed navbar from scrolling.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,16 +8,17 @@ function NavBar(props) {
         <h3>Dennis Francis</h3>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav ml-auto">
-            <a
+          <a
               className="nav-item"
-              href="#Contact"
-              onClick={() => props.handlePageChange("Contact")}
+              href="#About"
+              onClick={() => props.handlePageChange("About")}
               className={
-                props.currentPage === "Contact" ? "nav-link active" : "nav-link"
+                props.currentPage === "About" ? "nav-link active" : "nav-link"
               }
             >
-              Contact
+              About
             </a>
+            
             <a
               className="nav-item"
               href="#Portfolio"
@@ -30,17 +31,17 @@ function NavBar(props) {
             >
               Portfolio
             </a>
+            
             <a
               className="nav-item"
-              href="#About"
-              onClick={() => props.handlePageChange("About")}
+              href="#Contact"
+              onClick={() => props.handlePageChange("Contact")}
               className={
-                props.currentPage === "About" ? "nav-link active" : "nav-link"
+                props.currentPage === "Contact" ? "nav-link active" : "nav-link"
               }
             >
-              About
+              Contact
             </a>
-
             <a
               className="nav-link"
               href="https://docs.google.com/document/d/1MZ_OTMBpeteeMLyJbuGzgmT9NBKnAI1Qt-k5Pt69K1Y/edit?usp=sharing"
