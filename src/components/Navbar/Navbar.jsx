a1ab8253b03d9c1f7be32401194f65f9a4614451
Dennis Francis <dnfrancis22@gmail.com>
2021-02-24 19:14:23 -0500

    fixed responsiveness of the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,59 +8,65 @@ function NavBar(props) {
         className="navbar navbar-expand-lg navbar-light bg-light"
         id="navbar"
       >
+        <div className="container-fluid">
           <a
             className="nav-item"
             href="#Home"
             onClick={() => props.handlePageChange("Home")}
+            // eslint-disable-next-line
             className={
               props.currentPage === "Home" ? "nav-link active" : "nav-link"
             }
           >
             <h3>Dennis Francis</h3>
           </a>
-        
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ml-auto">
-            <a
-              className="nav-item"
-              href="#About"
-              onClick={() => props.handlePageChange("About")}
-              className={
-                props.currentPage === "About" ? "nav-link active" : "nav-link"
-              }
-            >
-              About
-            </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="navbar-nav ml-auto">
 
-            <a
-              className="nav-item"
-              href="#Portfolio"
-              onClick={() => props.handlePageChange("Portfolio")}
-              className={
-                props.currentPage === "Portfolio"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Portfolio
-            </a>
-            {/* <a
-              className="nav-item"
-              href="#Contact"
-              onClick={() => props.handlePageChange("Contact")}
-              className={
-                props.currentPage === "Contact" ? "nav-link active" : "nav-link"
-              }
-            >
-              Contact
-            </a> */}
-            <a
-              className="nav-link"
-              href="https://docs.google.com/document/d/1MZ_OTMBpeteeMLyJbuGzgmT9NBKnAI1Qt-k5Pt69K1Y/edit?usp=sharing"
-              download
-            >
-              Resume
-            </a>
+              <a
+                className="nav-item"
+                href="#About"
+                onClick={() => props.handlePageChange("About")}
+                // eslint-disable-next-line
+                className={
+                  props.currentPage === "About" ? "nav-link active" : "nav-link"
+                }
+              >
+                About
+              </a>
+
+              <a
+                className="nav-item"
+                href="#Portfolio"
+                onClick={() => props.handlePageChange("Portfolio")}
+                // eslint-disable-next-line
+                className={
+                  props.currentPage === "Portfolio"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Portfolio
+              </a>
+              <a
+                className="nav-link"
+                href="https://docs.google.com/document/d/1MZ_OTMBpeteeMLyJbuGzgmT9NBKnAI1Qt-k5Pt69K1Y/edit?usp=sharing"
+                download
+              >
+                Resume
+              </a>
+            </div>
           </div>
         </div>
       </nav>
